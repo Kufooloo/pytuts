5225d89c7400488bf7e179fc0f1d5398277bea2a
Kufooloo <robot2boy@icloud.com>
2020-01-23 13:33:11 -0800

    added settings and calendars

--- calendars_start.py
+++ calendars_start.py
@@ -3,13 +3,17 @@
 #
 
 # import the calendar module
-
+import calendar
 
 # create a plain text calendar
-
+c = calendar.TextCalendar(calendar.SUNDAY)
+str = c.formatmonth(2020,1,1,1)
+print(str)
 
 # create an HTML formatted calendar
-
+hc = calendar.HTMLCalendar(calendar.SUNDAY)
+str = hc.formatmonth(2020,1)
+print(str)
 
 # loop over the days of a month
 # zeroes mean that the day of the week is in an overlapping month
