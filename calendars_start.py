dca959bdfa071cf4b53fabd3bad52c61affb3197
Kufooloo <robot2boy@icloud.com>
2020-01-23 13:42:11 -0800

    re-added calendar and time_deltas

--- calendars_start.py
+++ calendars_start.py
@@ -7,17 +7,18 @@ import calendar
 
 # create a plain text calendar
 c = calendar.TextCalendar(calendar.SUNDAY)
-str = c.formatmonth(2020,1,1,1)
-print(str)
+# str = c.formatmonth(2020,1,1,1)
+# print(str)
 
 # create an HTML formatted calendar
-hc = calendar.HTMLCalendar(calendar.SUNDAY)
-str = hc.formatmonth(2020,1)
-print(str)
+# hc = calendar.HTMLCalendar(calendar.SUNDAY)
+# str = hc.formatmonth(2020,1)
+# print(str)
 
 # loop over the days of a month
 # zeroes mean that the day of the week is in an overlapping month
-
+# for i in c.itermonthdays(2020, 6):
+#     print (i) 
   
 # The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
@@ -27,4 +28,14 @@ print(str)
 # a team meeting on the first Friday of every month.
 # To figure out what days that would be for each month,
 # we can use this script:
-  
+print("Team meetings will be on:")
+for m in range(1, 13):
+    cal = calendar.monthcalendar(2020, m)
+    weekone = cal[0]
+    weektwo = cal[1]
+   
+    if weekone[calendar.FRIDAY] !=0:
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+    print("%10s %2d" % (calendar.month_name[m], meetday))
--- timedeltas_start.py
+++ timedeltas_start.py
@@ -9,28 +9,32 @@ from datetime import timedelta
 
 
 # construct a basic timedelta and print it
-print(timedelta(days = 365, hours = 5, minutes = 1))
+# print(timedelta(days = 365, hours = 5, minutes = 1))
 
 # print today's date
-now = datetime.now()
-print(str(now))
+# now = datetime.now()
+# print(str(now))
 
 # print today's date one year from now
-print(str(now + timedelta(days = 365)))
+#  print(str(now + timedelta(days = 365)))
 
 # create a timedelta that uses more than one argument
-print(str(now + timedelta(days = 3, weeks = 2, hours = 4)))
+# print(str(now + timedelta(days = 3, weeks = 2, hours = 4)))
 
 # calculate the date 1 week ago, formatted as a string
 
 
 ### How many days until April Fools' Day?
+today = date.day()
+afd = date(today.year, 4, 1)
 
 
 # use date comparison to see if April Fool's has already gone for this year
 # if it has, use the replace() function to get the date for next year
-
-
-# Now calculate the amount of time until April Fool's Day  
-
+if afd < today:
+    print ("April fools day already went by %d days ago" % ((today-afd).days))
+    afd = afd.replace(year=today.year +1)
+# # Now calculate the amount of time until April Fool's Day  
+time_to_afd = afd-today
+print("it's just", time_to_afd, "days until the next april fools day")
 
